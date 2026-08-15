7886522321d48e787b2122589ed9adf5af476dd9
Justin Kong <84046531+justinkong1@users.noreply.github.com>
2026-08-15 10:47:25 -0400

    Add: anagram-groups - submission-5

---
+++ Data Structures & Algorithms/anagram-groups/submission-5.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for(String s : strs){
+            int[] count = new int[26];
+            for(char c : s.toCharArray()) {
+                count[c-'a'] += 1;
+            }
+            String key = Arrays.toString(count);
+            if(map.containsKey(key)) {
+                map.get(key).add(s);
+            } else {
+                map.put(key, new ArrayList<>());
+                map.get(key).add(s);
+            }
+        }
+        return new ArrayList<>(map.values());
+    }
+}
