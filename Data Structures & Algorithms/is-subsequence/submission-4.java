b6ae4ae49ec7a8b08d0d4bb4efecce8e9793f861
Justin Kong <84046531+justinkong1@users.noreply.github.com>
2026-09-23 14:02:23 -0400

    Add: is-subsequence - submission-4

---
+++ Data Structures & Algorithms/is-subsequence/submission-4.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int[] pos = new int[s.length()];
+        int p=-1;
+        for(int i=0;i<s.length();i++){
+            boolean charFound=false;
+            while(p < t.length()-1) {
+                p++;
+                if(s.charAt(i) == t.charAt(p)) {
+                    charFound=true;
+                    break;
+                }
+            }
+            if(!charFound) {
+                return false;
+            }
+
+        }
+
+        return true;
+    }
+}
+
+/*
+node
+length: 4
+
+neetcode
+length: 8
+
+(n)eetc(o)(d)(e)
+
+search through the array 
+*/
